a8d70c0b142f4de1b148b2f86e9231a81675aa9b
pmb-optesis <pmb@optesis.com>
2020-01-16 15:41:41 +0000

    add module

---
+++ account_move_reversal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account Move Reversal',
+    'author': 'Optesis SA',
+    'version': '1.5.0',
+    'category': 'Tools',
+    'description': """
+    permet de faire une description ...
+""",
+    'summary': 'Module de ...',
+    'sequence': 9,
+    'depends': ['base', 'account'],
+    'data': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
