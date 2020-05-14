fdc53647150e1538a411c6b4d712092780de9946
optesis-pmb <44680745+optesis-pmb@users.noreply.github.com>
2020-05-14 09:25:16 +0000

    add payroll

--- l10n_pcgo/__manifest__.py
+++ l10n_pcgo/__manifest__.py
@@ -4,25 +4,21 @@
 # Copyright (c) Optesis 2018 www.optesis.com
 
 {
-    'name': 'Syscohada révisé',
-    'version': '12.1.2',
+    'name': 'Syscohada révisé ',
+    'version': '12.2',
     'author': 'Optesis',
     'category': 'Localization',
     'description': """
-
-New Accounting chart for OHADA counrtries.
-========================================================================
-
-Ce module permet de gérer le nouveau plan compable SYSCOHADA Révisé applicable à partir du 1er janvier 2018 pour tous les pays faisant partie de l'espace OHADA.
-
-
-**Credits:** cabinet d'expertise comptable www.kyriex.com.
-""",
-    'depends': ['base_iban', 'account','account_accountant', 'base_vat'],
+                    Ce module permet de gérer le nouveau plan compable SYSCOHADA Révisé.
+                    Ce module permet de gérer le nouveau plan compable SYSCOHADA Révisé
+                    applicable à partir du 1er janvier 2018 pour tous les pays faisant partie de l'espace OHADA.
+                    **Credits:** cabinet d'expertise comptable www.kyriex.com.
+                    """,
+    'website': 'http://www.optesis.com',
+    'depends': ['account','account_accountant'],
     'data': [
         'data/l10n_pcgo_chart_data.xml',
         'data/account_chart_template_data.xml',
-        'views/l10n_pcgo_view.xml',
         'data/account_tax_data.xml',
     ],
 
